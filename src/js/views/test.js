48ddedb940ac1a4a1dc21af1952d9f59e20f6349
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-12-03 19:38:26 +0000

    added lodash-es

--- src/js/views/test.js
+++ src/js/views/test.js
@@ -1,23 +1,32 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { useSpring, animated } from "react-spring";
-
+import range from "lodash-es/range";
 import { Context } from "../store/appContext";
 
 import "../../styles/demo.scss";
-
+const items = range(4);
+const interp = i => r => `translate3d(0, ${15 * Math.sin(r + (i * 2 * Math.PI) / 1.6)}px, 0)`;
 export const Test = () => {
 	const [count, setCount] = useState(12);
 	const { store, actions } = useContext(Context);
 	const [contentStatus, displayContent] = useState(false);
-	const contentProps = useSpring({
-		opacity: contentStatus ? 1 : 0,
-		marginTop: contentStatus ? 0 : -1000
+	// const contentProps = useSpring({
+	// 	opacity: contentStatus ? 1 : 0,
+	// 	marginTop: contentStatus ? 0 : -1000
+	// });
+	const { radians } = useSpring({
+		to: async next => {
+			while (1) await next({ radians: 2 * Math.PI });
+		},
+		from: { radians: 0 },
+		config: { duration: 3500 },
+		reset: true
 	});
 	return (
 		<div className="container">
 			<div>{count}</div>
-			<div className="button-container">
+			{/* <div className="button-container">
 				<button onClick={() => displayContent(a => !a)} className="button">
 					Toggle Content
 				</button>
@@ -29,7 +38,7 @@ export const Test = () => {
 				<animated.div className="box" style={contentProps}>
 					<h1>This content slid down. Thanks to React Spring</h1>
 				</animated.div>
-			)}
+			)} */}
 		</div>
 	);
 };
