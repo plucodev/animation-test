dcc61f735d5d443c7338ff2213c006f07418a60f
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-12-03 19:24:19 +0000

    fixed issue

--- src/js/views/test.js
+++ src/js/views/test.js
@@ -6,7 +6,13 @@ import { Context } from "../store/appContext";
 import "../../styles/demo.scss";
 
 export const Test = () => {
+	const [count, setCount] = useState(12);
 	const { store, actions } = useContext(Context);
+	const [contentStatus, displayContent] = useState(false);
 
-	return <div className="container">Test Page</div>;
+	return (
+		<div className="container">
+			<div>{count}</div>
+		</div>
+	);
 };
