95ff9b982d488296576e30efc8939d8a496861a9
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-12-03 19:31:57 +0000

    added spring toggle

--- src/js/views/test.js
+++ src/js/views/test.js
@@ -17,6 +17,19 @@ export const Test = () => {
 	return (
 		<div className="container">
 			<div>{count}</div>
+			<div className="button-container">
+				<button onClick={() => displayContent(a => !a)} className="button">
+					Toggle Content
+				</button>
+			</div>
+			{!contentStatus ? (
+				<div>No Content</div>
+			) : (
+				// Here's where the animated hook comes into play
+				<animated.div className="box" style={contentProps}>
+					<h1>This content slid down. Thanks to React Spring</h1>
+				</animated.div>
+			)}
 		</div>
 	);
 };
