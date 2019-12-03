e7f76e8b667e8f5fedbdb01d3f9705df8d865075
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-12-03 19:21:04 +0000

    created test component

---
+++ src/js/views/test.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+import "../../styles/demo.scss";
+
+export const Test = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="container">
+			Test Page
+		</div>
+	);
+};
